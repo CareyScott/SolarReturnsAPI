5e6bb3dae05822e5db9b6b95999c7f8a54f307f2
CareyScott <yeracsott9>
2022-09-27 11:14:16 +0100

    making changes

--- controllers/solar_controller.js
+++ controllers/solar_controller.js
@@ -26,14 +26,12 @@ const solar_request = async (req, res) => {
         },
       })
       .then((response) => {
-        
         solarRequestData = response.data;
       });
-      
   } catch (err) {
     console.log(err);
   }
-  return solarRequestData
+  return solarRequestData;
 };
 
 // Function to calculate all key outputs
@@ -66,64 +64,82 @@ const data_extraction_sum = async (req, res) => {
   var initial_prod = 0;
   var monthly_production = [];
   for (let i = 0; i < newArray.length; i++) {
-    monthly_production[i] = Math.round(Number(newArray[i].E_m)*100)/100;
-    initial_prod = Math.round((initial_prod + Number(newArray[i].E_m))*100)/100;
+    monthly_production[i] = Math.round(Number(newArray[i].E_m) * 100) / 100;
+    initial_prod =
+      Math.round((initial_prod + Number(newArray[i].E_m)) * 100) / 100;
   }
 
   // Calculate annual production including panel degration
   var annual_prod = new Array(system_life);
   for (let i = 0; i < system_life; i++) {
-    annual_prod[i] = Math.round(initial_prod*((1-panel_degradation_rate)**i)*100)/100
+    annual_prod[i] =
+      Math.round(initial_prod * (1 - panel_degradation_rate) ** i * 100) / 100;
   }
-  
+
   // Calculate Projected Electricity Costs
   let elec_day_proj = [];
   let elec_night_proj = [];
   let elec_sale_proj = [];
 
   for (let i = 0; i < system_life; i++) {
-    elec_day_proj[i] = Math.round((current_day_elec_price*(1+inflation_rate+elec_price_increase_rate)**(i))*1000)/1000;
-    elec_night_proj[i] = Math.round((current_night_elec_price*(1+inflation_rate+elec_price_increase_rate)**(i))*1000)/1000;
-    elec_sale_proj[i] = Math.round((current_elec_sale_price*(1+inflation_rate)**(i))*1000)/1000;
+    elec_day_proj[i] =
+      Math.round(
+        current_day_elec_price *
+          (1 + inflation_rate + elec_price_increase_rate) ** i *
+          1000
+      ) / 1000;
+    elec_night_proj[i] =
+      Math.round(
+        current_night_elec_price *
+          (1 + inflation_rate + elec_price_increase_rate) ** i *
+          1000
+      ) / 1000;
+    elec_sale_proj[i] =
+      Math.round(current_elec_sale_price * (1 + inflation_rate) ** i * 1000) /
+      1000;
   }
 
   // Calculate the Simple Payback in Years
   // Calculate outgoing expenses
   let outgoing = new Array(system_life);
-  for (let i=0; i<(system_life); i++){ 
+  for (let i = 0; i < system_life; i++) {
     outgoing[i] = 0;
   }
   outgoing[0] = system_cost_inc_grant;
 
-  for (let i = 1; i <= Math.floor(system_life/inverter_life); i++){
-    outgoing[inverter_life*i]=inverter_cost;
+  for (let i = 1; i <= Math.floor(system_life / inverter_life); i++) {
+    outgoing[inverter_life * i] = inverter_cost;
   }
 
   // Calculate Incoming Revenue
   let incoming = new Array(system_life);
-  for (let i=0; i<(system_life); i++){ 
+  for (let i = 0; i < system_life; i++) {
     incoming[i] = 0;
   }
-  for (let i=0; i<(system_life); i++){ 
-    incoming[i] = (annual_prod[i]*self_consumption)*elec_day_proj[i] + (annual_prod[i]*(1-self_consumption))*elec_sale_proj[i];
+  for (let i = 0; i < system_life; i++) {
+    incoming[i] =
+      annual_prod[i] * self_consumption * elec_day_proj[i] +
+      annual_prod[i] * (1 - self_consumption) * elec_sale_proj[i];
   }
-  
+
   // Cumulative Costs/Income
   let simple_cumulative = new Array(system_life);
   var simple_payback = 0;
-  
-  for (let i=0; i<(system_life); i++){ 
-    if (i == 0){
-      simple_cumulative[0] = Math.round(incoming[0]-outgoing[0]);
+
+  for (let i = 0; i < system_life; i++) {
+    if (i == 0) {
+      simple_cumulative[0] = Math.round(incoming[0] - outgoing[0]);
     } else {
-      simple_cumulative[i] = Math.round(simple_cumulative[i-1]+incoming[i]-outgoing[i]);
+      simple_cumulative[i] = Math.round(
+        simple_cumulative[i - 1] + incoming[i] - outgoing[i]
+      );
     }
   }
-  for (let i=0; i<(system_life); i++){ 
-    if (simple_cumulative[i]>=0){
-      simple_payback = i+1;
-      break
-    } 
+  for (let i = 0; i < system_life; i++) {
+    if (simple_cumulative[i] >= 0) {
+      simple_payback = i + 1;
+      break;
+    }
   }
 
   // Calculate Net Present Value of Investment
@@ -133,31 +149,33 @@ const data_extraction_sum = async (req, res) => {
   let discount_incoming = new Array(system_life);
   var net_present_value = 0;
 
-  for (let i=0; i<(system_life); i++){ 
-    discount_outgoing[i] = outgoing[i]/(1+discount_rate)**i;
-    discount_incoming[i] = incoming[i]/(1+discount_rate)**(i+1);
-    net_present_value = Math.round(net_present_value - discount_outgoing[i] + discount_incoming[i])
+  for (let i = 0; i < system_life; i++) {
+    discount_outgoing[i] = outgoing[i] / (1 + discount_rate) ** i;
+    discount_incoming[i] = incoming[i] / (1 + discount_rate) ** (i + 1);
+    net_present_value = Math.round(
+      net_present_value - discount_outgoing[i] + discount_incoming[i]
+    );
   }
 
-  // key outputs that will need to be returned to the front end are; monthly_production (array), 
-  // annual_prod (int), incoming (array), outgoing (array), simple_cumulative (array), 
+  // key outputs that will need to be returned to the front end are; monthly_production (array),
+  // annual_prod (int), incoming (array), outgoing (array), simple_cumulative (array),
   // simple_payback (int),discount_outgoing (array), discount_incoming(array), net_present_value(int)
-  const json_calc_results = {
-    "monthly_production":JSON.stringify(monthly_production),
-    "annual_production":JSON.stringify(annual_prod),
-    "simple_cumulative":JSON.stringify(simple_cumulative),
-    "simple_payback":JSON.stringify(simple_payback),
-    "net_present_value":JSON.stringify(net_present_value),
-  }
-  
+  let json_calc_results = {
+    monthly_production: monthly_production,
+    annual_production: annual_prod,
+    simple_cumulative: simple_cumulative,
+    simple_payback: simple_payback,
+    net_present_value: net_present_value,
+  };
+
   // console.log(json_calc_results);
   // console.log(simple_payback);
   // console.log(net_present_value);
   // console.log(initial_prod);
- 
+
   if (json_calc_results) {
     // filters predictions probability threshold
-    
+
     // return predicted array
     res.status(200).json(json_calc_results);
     console.log("done");
@@ -168,10 +186,9 @@ const data_extraction_sum = async (req, res) => {
   //   console.error(err);
   //   res.status(500).json(err);
   // });
-
-}
+};
 
 module.exports = {
   solar_request,
-  data_extraction_sum
-}
+  data_extraction_sum,
+};
